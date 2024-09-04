67a07e2aa9eb8523514ba57da5f25ad045baa37e
accinn <aylinpt02@gmail.com>
2024-09-04 17:52:39 -0300

    Carpeta Agregada con proyecto sin nada

--- Categoria/Elemento.cs
+++ Categoria/Elemento.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Categoria
 {
-    public class Class1
+    public class Elemento
     {
     }
 }
--- Marca/Marca.cs
+++ Marca/Marca.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Marca
 {
-    public class Class1
+    public class Marca
     {
     }
 }
